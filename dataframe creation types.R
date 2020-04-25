446524e34afadd8808612c351fb22c1b5ad7edcb
Md. Ridone Ahmed <34868317+r1ridwan@users.noreply.github.com>
2020-04-26 02:26:43 +0600

    Update dataframe creation types.R

--- dataframe creation types.R
+++ dataframe creation types.R
@@ -16,3 +16,28 @@ print(summary(emp.data))
 #Extract Specific columns.
 result <- data.frame(emp.data$emp_name,emp.data$salary) 
 print(result)
+#Extract first two rows.
+result <- emp.data[1:2,]
+print(result)
+# Extract 3rd and 5th row with 2nd and 4th column.
+result <- emp.data[c(3,5),c(2,4)]
+print(result)
+# New Colum Addition/ Add the "dept" coulmn.
+emp.data$dept <- c("IT","Operations","IT","HR","Finance")
+v <- emp.data
+print(v)
+# Create the second data frame/ To add more rows permanently to an existing data frame, we need to bring in the new rows
+#in the same structure as the existing data frame and use the rbind() function.
+#In the example below we create a data frame with new rows and merge it with the existing data frame to create the final data frame.
+emp.newdata <- 	data.frame(
+  emp_id = c (6:8), 
+  emp_name = c("Rasmi","Pranab","Tusar"),
+  salary = c(578.0,722.5,632.8), 
+  start_date = as.Date(c("2013-05-21","2013-07-30","2014-06-17")),
+  dept = c("IT","Operations","Fianance"),
+  stringsAsFactors = FALSE
+)
+# Bind the two data frames.
+emp.finaldata <- rbind(emp.data,emp.newdata)
+print(emp.finaldata)
+

ad8b0d4d878d284010db2fb112d2af2c1da1d201
Md. Ridone Ahmed <34868317+r1ridwan@users.noreply.github.com>
2020-06-15 13:15:50 +0600

    Update Vectors.R

--- Data Structures or Types/Vectors.R
+++ Data Structures or Types/Vectors.R
@@ -7,7 +7,7 @@ length(x)
 =============================================================================================
 # LinK: https://www.tutorialspoint.com/r/r_vectors.htm
 
-->SINGLE ELEMENT VECTOR
+==>SINGLE ELEMENT VECTOR
 typeof("abc") # Atomic vector of type character.
 typeof(12.5)# Atomic vector of type double.
 typeof(63L)# Atomic vector of type integer.
@@ -15,16 +15,16 @@ typeof(TRUE)# Atomic vector of type logical.
 typeof(2+4i)# Atomic vector of type complex.
 typeof(charToRaw('hello'))# Atomic vector of type raw.
 
-->MULTIPLE ELEMENTS VECTOR
+==>MULTIPLE ELEMENTS VECTOR
 v <- 5:13
 print(v)
 v <- 3.8:11.4
 print(v)
 
-->Using sequence (Seq.) operator
+==>Using sequence (Seq.) operator
 print(seq(5,9, by=0.4))#Create vector with elements from 5 to 9 incrementing by 0.4
 
-->ACCESSING VECTOR ELEMENTS
+==>ACCESSING VECTOR ELEMENTS
 t <- c("Sun","Mon","Tue","Wed","Thurs","Fri","Sat")#Accessing vector elements using position
 u <- t[c(3,1,7)]
 print(u)
@@ -33,7 +33,7 @@ print(v)
 y <- t[c(1,0,0,0,0,0,7)]# Accessing vector elements using 0/1 indexing.
 print(y)
 
-->Vector Manipulation
+==>Vector Manipulation
 v1 <- c(3,8,4,5,0,11)
 v2 <- c(4,11,0,8,1,2)
 add.result<- v1+v2 #Vector Addition 

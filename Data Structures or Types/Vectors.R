48fbfe73046910507feedd390ffc48bb95bfce47
Md. Ridone Ahmed <34868317+r1ridwan@users.noreply.github.com>
2020-06-15 14:10:09 +0600

    Update Vectors.R

--- Data Structures or Types/Vectors.R
+++ Data Structures or Types/Vectors.R
@@ -1,19 +1,27 @@
 #Here we will learn about all data structures
+Vectors are the most basic R data objects and there are six types of atomic vectors. They are logical, integer, double, complex, 
+character and raw.
 library(tidyverse)
 typeof(letters)
 typeof(1:12)
 x <- list("a", "b", 1:10)
 length(x)
-=============================================================================================
+======================================================================================================
 # LinK: https://www.tutorialspoint.com/r/r_vectors.htm
 
+==>Combining Vectors: http://www.r-tutor.com/r-introduction/vector/combining-vectors
+n= c(2, 3, 5) 
+s= c("aa", "bb", "cc", "dd", "ee") 
+combined=c( s,  n)
+print(combined)
+
 ==>SINGLE ELEMENT VECTOR
-typeof("abc") # Atomic vector of type character.
-typeof(12.5)# Atomic vector of type double.
-typeof(63L)# Atomic vector of type integer.
-typeof(TRUE)# Atomic vector of type logical.
-typeof(2+4i)# Atomic vector of type complex.
-typeof(charToRaw('hello'))# Atomic vector of type raw.
+typeof("abc") # Atomic vector of type CHARACTER
+typeof(12.5)# Atomic vector of type DOUBLE
+typeof(63L)# Atomic vector of type INTEGER
+typeof(TRUE)# Atomic vector of type LOGICAL
+typeof(2+4i)# Atomic vector of type COMPLEX
+typeof(charToRaw('hello'))# Atomic vector of type RAW
 
 ==>MULTIPLE ELEMENTS VECTOR
 v <- 5:13
@@ -33,7 +41,7 @@ print(v)
 y <- t[c(1,0,0,0,0,0,7)]# Accessing vector elements using 0/1 indexing.
 print(y)
 
-==>Vector Manipulation
+==>VECTOR MANUPULATION
 v1 <- c(3,8,4,5,0,11)
 v2 <- c(4,11,0,8,1,2)
 add.result<- v1+v2 #Vector Addition 
@@ -45,5 +53,31 @@ print(multi.result)
 divi.result<-v1/v2 #Vector Division 
 print(divi.result)
 
-
+==>VECTOR RECYCLING
+v1<- c(3, 4, 5, 6, 7, 8, 9)
+v2<-c(4, 11)# v2 becomes (4,11,4,11,4,11,4)
+add.result<- v1+v2
+print(add.result)
+sub.result<-v1-v2
+print(sub.result)
+========================================================================================================================
+===>VECTOR ELEMENTS SORTING
+v<- c(3,8,4,5,0,11, -9, 304)
+sort.result<- sort(v)
+print(sort.result)
+revsort.result<- sort(v, decreasing = TRUE)
+print(revsort.result)
+------------------------------------------------------
+v<-c("Blue", "Red", "Yellow", "violet", "Green")
+sot.resutl<-sort(v)
+print(sot.resutl)
+revsort.result<-sort(v, decreasing = FALSE)
+print(revsort.result)
+revsort.result<-sort(v, decreasing = TRUE)
+print(revsort.result)
+When we execute the above code, it produces the following result −
+[1]  -9   0   3   4   5   8  11 304
+[1] 304  11   8   5   4   3   0  -9
+[1] "Blue"   "Red"    "violet" "yellow"
+[1] "yellow" "violet" "Red"    "Blue"
 

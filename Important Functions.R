07637cb36990a56ec51536d02af62b26487a97cc
Md. Ridone Ahmed <34868317+r1ridwan@users.noreply.github.com>
2020-08-23 23:06:21 +0600

    Update Important Functions.R

--- Important Functions.R
+++ Important Functions.R
@@ -3,5 +3,12 @@ getwd()
 #Set New Working Directory
 setwd("I:/Bioinformatics/Scripts")
 =========================================
-If you want to check what things you have done througout the full session at a particular time, you should use 
+#If you want to check what things you have done througout the full session at a particular time, you should use 
 sessionInfo()
+==================================================
+#TO SEE THE STRUCTURE OF DATA
+str(datafilename)
+=====================================================
+#TO SEE THE NUMBER OF ROWS IN DATA
+nrow(datafilename)
+=======================================================

0f62627eb8d081064991d41c1293779f421302ca
Md. Ridwan Ahmed <34868317+r1ridwan@users.noreply.github.com>
2022-01-01 21:52:25 +0600

    hfjgfgfgj

--- Important Functions.R
+++ Important Functions.R
@@ -3,21 +3,26 @@ getwd()
 #Set New Working Directory
 setwd("I:/Bioinformatics/Scripts")
 =========================================
+
 #If you want to check what things you have done througout the full session at a particular time, you should use 
 sessionInfo()
 ==================================================
+
 #TO SEE THE STRUCTURE OF DATA
 str(datafilename)
 =====================================================
+
 #TO SEE THE NUMBER OF ROWS IN DATA
 nrow(datafilename)
 ==================================================
+
 # CHECK  VERSIONS (Bioconductor, Packages, R)
 R.version
 version()
 packageVersion("BiocManager")
 packageVersion("dplyr")
 ====================================================
+
 # HOW TO GET HELP FOR ANY PACKAGES? HOW TO SEE R DOCUMENTATION FOR A PACKAGE?
 ?pacakgename
 help(packagename)
@@ -26,3 +31,11 @@ browseVignettes("packagename")
 help(BSgenome)
 browseVignettes("BSgenome")
 ==============================================================
+
+# WHEN YOU WANT TO KNOW ABOUT A PARTICULAR FUNCTION, JUST DO THIS FILLOWING WAY-
+packagesname::functionname
+
+
+
+
+
